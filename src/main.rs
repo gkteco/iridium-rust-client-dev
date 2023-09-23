60fc121221a4c601f92a0bb22a66be967b304715
Gianni Crivello <gianni.crivllo@techolution.com>
2023-09-23 11:48:47 -0400

    updated links

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 
-mod auth_url_service;
 mod callback_service;
 mod token_service;
 mod state_generator_service;
 mod url_generator_service;
 
-use auth_url_service::AuthUrlService;
-use callback_service::CallBackService;
-use token_service::TokenService;
 use tokio;
 use warp::{Filter, http::StatusCode, Reply};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::str::FromStr;
 use warp::http::Uri;
-use warp::reply::Response;
-use crate::auth_url_service::AuthUrlService::get_iridium_auth_url;
 use crate::callback_service::CallBackService::handle_callback;
 use crate::state_generator_service::StateGenerator;
 use crate::url_generator_service::UrlGeneratorService::getIridiumAuthUrl;
