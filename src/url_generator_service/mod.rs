f5119d60bc51245acdb322dbf9e6a266111dd266
gianni <gianniemperado@gmail.com>
2023-09-23 12:18:56 -0400

    test

--- src/url_generator_service/mod.rs
+++ src/url_generator_service/mod.rs
@@ -8,9 +8,9 @@ pub mod UrlGeneratorService {
         dotenv().ok();
 
         let base_url = env::var("IRIDIUM_BASE_URL");
-        let redirectUri = env::var("NEXT_PUBLIC_IRIDIUM_REDIRECT_URI");
-        let clientId = env::var("NEXT_PUBLIC_IRIDIUM_CLIENT_ID");
+        let redirectUri = env::var("RUST_PUBLIC_IRIDIUM_REDIRECT_URI");
+        let clientId = env::var("RUST_PUBLIC_IRIDIUM_CLIENT_ID");
 
-        format!("{}login?response_type=code&state={}&redirect_uri={}&client_id={}", base_url.unwrap(), redirectUri.unwrap(), clientId.unwrap(), state)
+        format!("{}/login?response_type=code&state={}&redirect_uri={}&client_id={}", base_url.unwrap(), state, redirectUri.unwrap(), clientId.unwrap())
     }
 }
